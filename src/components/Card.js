e5e934a85b420c51d824430a906d2bd5a781d32a
KingBenny101 <benstin2727@gmail.com>
2021-09-04 15:26:34 +0530

    Update 1.3.0

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,30 +5,30 @@ function Card({handle}) {
     <div className="card">
       <div className="dcmSelectContainer">
         <div className="rowContainer">
-          <div className="bubble">
-            <span className="bubbleText" onClick={()=>handle(0)}> Measurement</span>
+          <div className="bubble" onClick={()=>handle(0)}>
+            <span className="bubbleText" > Measurement</span>
           </div>
         </div>
         <div className="rowContainer">
-          <div className="bubble">
-            <span className="bubbleText" onClick={()=>handle(1)}>Laws of Motion</span>
+          <div className="bubble" onClick={()=>handle(1)}>
+            <span className="bubbleText" >Laws of Motion</span>
           </div>
 
           <span className="titleText">
             Digital <br />
             Concept Map
           </span>
-          <div className="bubble">
-            <span className="bubbleText" onClick={()=>handle(2)}>Current and Electricity</span>
+          <div className="bubble" onClick={()=>handle(2)}>
+            <span className="bubbleText" >Current and Electricity</span>
           </div>
         </div>
         <div className="rowContainer">
-          <div className="bubble">
-            <span className="bubbleText" onClick={()=>handle(3)}> Light</span>
+          <div className="bubble" onClick={()=>handle(3)}>
+            <span className="bubbleText" > Light</span>
           </div>
           <div className="bubble invisibleBubble"></div>
-          <div className="bubble">
-            <span className="bubbleText" onClick={()=>handle(4)}>Sound</span>
+          <div className="bubble" onClick={()=>handle(4)}>
+            <span className="bubbleText" >Sound</span>
           </div>
         </div>
       </div>
--- src/components/Frame.js
+++ src/components/Frame.js
@@ -3,9 +3,15 @@ import Iframe from "react-iframe";
 
 
 function Frame({url}){
+
+    function returnHome (){
+        window.location.reload();
+    }
+
     return(
         <div className="frame" >
             <Iframe url = {url}></Iframe>
+            <div id= "home" onClick = {()=>returnHome()}></div>
         </div>
     )
 }
